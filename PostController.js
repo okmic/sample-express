dcececc1e8f4f281e825955561777529b336fc3a
Michael <alpha.lucky.m@gmail.com>
2021-12-31 14:17:23 +0300

    CRUD

--- PostController.js
+++ PostController.js
@@ -12,16 +12,52 @@ class PostController {
         }
     }
     async getAll(req, res) {
-
+        try {
+            const posts = await Post.find()
+            return res.json(posts)
+        }
+        catch(e) {
+            res.status(500).json(e)
+        }
     }
     async getOne(req, res) {
-
+        try {
+            const {id} = req.params
+            if (!id){
+                res.status(400).json({message: "Id not specified"})
+            }
+            const post = await Post.findById(id)
+            return res.json(post)
+        }
+        catch(e) {
+            res.status(500).json(e)
+        }
     }
     async update(req, res) {
-
+        try {
+            const post = req.body
+            if(!post._id){
+                res.status(400).json({message: "Id not specified"})
+            }
+            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true})
+            return res.json(updatedPost)
+        }
+        catch(e) {
+            res.status(500).json(e)
+        }
     }
     async delete(req, res) {
-
+        try {
+            const {id} = req.params
+            if(!id){
+                res.status(400).json({message: "Id not specified"})
+            }
+            const post = await Post.findByIdAndDelete(id)
+            return res.json(post)
+        }
+        catch(e) {
+            res.status(500).json(e)
+        }
     }
 }
 
--- router.js
+++ router.js
@@ -5,9 +5,9 @@ import PostController from "./PostController.js"
 const router = new Router()
 
 router.post('/posts',  PostController.create)
-router.get('/posts')
-router.get('/posts/:id')
-router.put('/posts')
-router.delete('/posts/:id')
+router.get('/posts', PostController.getAll)
+router.get('/posts/:id', PostController.getOne)
+router.put('/posts', PostController.update)
+router.delete('/posts/:id', PostController.delete)
 
 export default router
